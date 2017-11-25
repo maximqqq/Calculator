727b1ca3d3b803fbabd059ab86ee3b2b4fdf1b0d
Max <https://github.com/maximqqq>
2017-11-25 16:29:11 +0300

    1

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
+
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+
+
+
 
 
 
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Calculator extends JFrame {
 
@@ -13,12 +14,92 @@ public class Calculator extends JFrame {
     public Calculator()
     {
         this.setTitle("КАЛЬКУЛЯТОР");
-        this.setSize(500,500);
+        this.setSize(300,400);
         this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+////////////////////////////////////////////////////////////////////////////////////////
+        setLayout(new BorderLayout());
+
+        JMenuBar menuBar =new JMenuBar();
+
+        JMenu menu = new JMenu();
+        menu.setText("Файл");
 
 
+        JMenuItem open = new JMenuItem();
+        open.setText("Открыть");
+        JMenuItem save = new JMenuItem();
+        save.setText("Сохранить");
+        JMenuItem close = new JMenuItem();
+        close.setText("Закрыть");
+        JSeparator separator = new JSeparator();
+        JMenuItem exit = new JMenuItem();
+        exit.setText("Выйти");
+
+        menu.add(open);
+        menu.add(save);
+        menu.add(close);
+        menu.add(exit);
+
+        menuBar.add(menu);
+
+        add(menuBar,BorderLayout.NORTH);//добавляем элементы в саму форму
+//////////////////////////////////////////////////////////////////////////////////////////
+        JMenu options = new JMenu();
+        options.setText("Опции");
+
+        ButtonGroup group = new ButtonGroup();
+
+
+        JRadioButtonMenuItem plus = new JRadioButtonMenuItem("+",false);
+        group.add(plus);
+        JRadioButtonMenuItem minus = new JRadioButtonMenuItem("-",true);
+        group.add(minus);
+        JRadioButtonMenuItem umnojit = new JRadioButtonMenuItem("*",false);
+        group.add(umnojit);
+        JRadioButtonMenuItem delit = new JRadioButtonMenuItem("/",false);
+        group.add(delit);
+
+        options.add(plus);
+        options.add(minus);
+        options.add(umnojit);
+        options.add(delit);
+
+        menuBar.add(options);
+//////////////////////////////////////////////////////////////////////////////////////////
+
+        JPanel panel = new JPanel();
+       // panel.setSize(50,50);
+
+        JTextArea textArea = new JTextArea(5,20);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setSize(10,10);
+        add(panel,BorderLayout.SOUTH);
+        panel.add(textArea);
+
+        JButton button1 = new JButton("=");
+
+        button1.setSize(10,20);
+
+        panel.add(button1);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+        this.setVisible(true);
     }
+
 
 
 
